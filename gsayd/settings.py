44f7863d31684c391aadd9f9afe3187d9043d04c
prashantsagar73 <prashant.sagar73@gmail.com>
2021-09-18 19:14:32 +0530

    home frontend

--- gsayd/settings.py
+++ gsayd/settings.py
@@ -123,8 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "statics"),
+   BASE_DIR, "static",
    ]
 
 # media files 
