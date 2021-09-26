c6f31e259316a6bb403cc6c7f3d28494e629c510
prashantsagar73 <prashant.sagar73@gmail.com>
2021-09-26 12:59:15 +0530

    bugs fixed in events urls

--- gsayd/urls.py
+++ gsayd/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
     path('profile/', user_views.profile,name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('',include('home.urls')),
-    path('events/',include('events.urls')),
+    path('',include('home.urls'), name='Home'),
+    path('events/',include('events.urls'), name="event_home"),
 
 ]
 
